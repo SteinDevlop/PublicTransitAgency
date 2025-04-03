427e908ae5501762b75b7dbd37433c19aeeba6a8
DEV-ZERO-cloud <andresrubianomarrugo@gmail.com>
2025-04-02 23:26:02 -0500

    Uploding Class about Users

--- src/backend/app/services/user.py
+++ src/backend/app/services/user.py
@@ -1,9 +1,10 @@
 class User:
-    def __init__(self, id_user: int, name: str, email: str, password: str):
+    def __init__(self, id_user: int, name: str, email: str, password: str, role: str):
         self.id_user = id_user
         self.name = name
         self.email = email
         self.password = password
+        self.role = role
 
     def use_card(self):
         raise NotImplementedError("The use_card method must be implemented by subclasses.")
---
+++ src/backend/app/services/user_passenger.py
@@ -0,0 +1,9 @@
+from user import User
+
+class Passenger(User):
+    def __init__(id_user, name, email, password, role):
+        super().__init__(id_user, name, email, password, role)
+
+    def get_information(self):
+            print(f"Information: ID:{self.id_user},Name:{self.name}, Email:{self.email}, Password:{self.password}, Role:{self.role}")
+            return True
