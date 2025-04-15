6dd84813dc8635caf058c90eb9afe6890029fc44
Sinoombre <mjuliowilches@gmail.com>
2025-04-15 13:37:45 -0500

    Update ticket.py

--- src/backend/app/logic/ticket.py
+++ src/backend/app/logic/ticket.py
@@ -3,8 +3,9 @@ class Ticket:
     Clase que encapsula el estado de un ticket como un número entero (1, 2 o 3).
     """
 
-    def __init__(self, status_code: int):
+    def __init__(self, status_code: int, ID : str):
         self._status_code = status_code
+        self._ID = ID
 
     @property
     def status_code(self) -> int:
