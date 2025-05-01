d489731718b6504985ab228ab6472a104a2e73a4
Sinoombre <mjuliowilches@gmail.com>
2025-05-01 16:02:31 -0500

    import

--- src/backend/app/test/logic/test_incidence.py
+++ src/backend/app/test/logic/test_incidence.py
@@ -5,40 +5,40 @@ class TestIncidence(unittest.TestCase):
 
     def setUp(self):
         self.incidence_data = {
-            "Descripcion": "Descripción de la incidencia",
-            "Tipo": "Tipo de incidencia",
-            "TicketID": 10
+            "descripcion": "Descripción de la incidencia",
+            "tipo": "Tipo de incidencia",
+            "ticket_id": 10
         }
         self.incidence = IncidenceCreate(**self.incidence_data)
 
     def test_initial_values(self):
-        self.assertEqual(self.incidence.Descripcion, "Descripción de la incidencia")
-        self.assertEqual(self.incidence.Tipo, "Tipo de incidencia")
-        self.assertEqual(self.incidence.TicketID, 10)
+        self.assertEqual(self.incidence.descripcion, "Descripción de la incidencia")
+        self.assertEqual(self.incidence.tipo, "Tipo de incidencia")
+        self.assertEqual(self.incidence.ticket_id, 10)
 
     def test_setters(self):
-        self.incidence.Descripcion = "Nueva descripción"
-        self.incidence.Tipo = "Nuevo tipo"
-        self.incidence.TicketID = 20
+        self.incidence.descripcion = "Nueva descripción"
+        self.incidence.tipo = "Nuevo tipo"
+        self.incidence.ticket_id = 20
 
-        self.assertEqual(self.incidence.Descripcion, "Nueva descripción")
-        self.assertEqual(self.incidence.Tipo, "Nuevo tipo")
-        self.assertEqual(self.incidence.TicketID, 20)
+        self.assertEqual(self.incidence.descripcion, "Nueva descripción")
+        self.assertEqual(self.incidence.tipo, "Nuevo tipo")
+        self.assertEqual(self.incidence.ticket_id, 20)
 
     def test_to_dict(self):
         expected_dict = {
-            "Descripcion": "Descripción de la incidencia",
-            "Tipo": "Tipo de incidencia",
-            "TicketID": 10
+            "descripcion": "Descripción de la incidencia",
+            "tipo": "Tipo de incidencia",
+            "ticket_id": 10
         }
         self.assertEqual(self.incidence.to_dict(), expected_dict)
 
     def test_get_fields(self):
         expected_fields = {
-            "IncidenciaID": "INTEGER PRIMARY KEY",
-            "Descripcion": "TEXT NOT NULL",
-            "Tipo": "TEXT",
-            "TicketID": "INTEGER NOT NULL"
+            "incidencia_id": "INTEGER PRIMARY KEY",
+            "descripcion": "TEXT NOT NULL",
+            "tipo": "TEXT",
+            "ticket_id": "INTEGER NOT NULL"
         }
         self.assertEqual(IncidenceCreate.get_fields(), expected_fields)
 
