0cedc3b1000f43629e4ecb07de85c73b5f7d6e1b
Sinoombre <mjuliowilches@gmail.com>
2025-05-01 17:42:38 -0500

    final fix?

--- src/backend/app/models/incidence.py
+++ src/backend/app/models/incidence.py
@@ -1,17 +1,12 @@
-"""from typing import Optional
+from typing import Optional
 from pydantic import BaseModel
 
 class Incidence(BaseModel):
     __entity_name__ = "incidence"
     incidence_id: Optional[int] = None
     description: str
-<<<<<<< HEAD
-    status: str  # Relacionado con Ticket
-    type: str
-=======
     type: str
     status: str
->>>>>>> e4587d1 (changes to incidence logic)
 
     def to_dict(self):
         return self.model_dump()
@@ -20,14 +15,7 @@ class Incidence(BaseModel):
     def get_fields(cls):
         return {
             "incidence_id": "INTEGER PRIMARY KEY",
-<<<<<<< HEAD
-            "description": "TEXT NOT NULL",
-            "status": "TEXT NOT NULL",
-            "type": "TEXT"
-        }"""
-=======
             "description": "TEXT",
             "type": "TEXT",
             "status": "TEXT"
         }
->>>>>>> e4587d1 (changes to incidence logic)
