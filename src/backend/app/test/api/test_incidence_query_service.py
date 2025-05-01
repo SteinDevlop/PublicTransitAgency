242f3bbb8489cf5eeab84823fec4617232b8475a
Sinoombre <mjuliowilches@gmail.com>
2025-04-30 20:57:17 -0500

    Pls?

--- src/backend/app/test/api/test_incidence_query_service.py
+++ src/backend/app/test/api/test_incidence_query_service.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from backend.app.api.routes.incidence_query_service import app as incidence_router
 from backend.app.logic.universal_controller_sql import UniversalController
-from backend.app.models.incidence import IncidenceCreate
+from backend.app.models.incidence import IncidenceCreate, IncidenceOut  # Importar IncidenceOut
 
 # Limpieza de base de datos antes y después de cada test
 def setup_function():
@@ -34,6 +34,7 @@ def test_get_all_incidences():
     assert len(data) >= 2
     assert data[0]["Descripcion"] in ["Incidencia1", "Incidencia2"]
     assert data[0]["Tipo"] in ["Tipo1", "Tipo2"]
+    assert data[0]["TicketID"] in [5, 6] #verifico tambien el ticketID
 
 def test_get_incidence_by_id_existing():
     """Prueba que la ruta '/incidencia/{IncidenciaID}' devuelve la incidencia correcta cuando existe."""
