8f4fbc3ef420d05fd013ee469c63f0dc4ead2af3
Sinoombre <mjuliowilches@gmail.com>
2025-05-01 16:53:16 -0500

    +1

--- src/backend/app/api/routes/incidence_query_service.py
+++ src/backend/app/api/routes/incidence_query_service.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, Form, Request, HTTPException, APIRouter, Query
+from fastapi import FastAPI, Request, HTTPException, APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from backend.app.models.incidence import IncidenceOut, Incidence
+from backend.app.models.incidence import Incidence
 from backend.app.logic.universal_controller_sql import UniversalController
-from typing import List, Optional
 
 app = APIRouter(prefix="/incidences", tags=["incidences"])
 controller = UniversalController()
