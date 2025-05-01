6bd3817d1051e760413f111f0997b7fa69288485
Sinoombre <mjuliowilches@gmail.com>
2025-05-01 16:27:46 -0500

    i'm an idiot

--- src/backend/app/api/routes/incidence_query_service.py
+++ src/backend/app/api/routes/incidence_query_service.py
@@ -1,4 +1,4 @@
-"""from fastapi import FastAPI, Form, Request, HTTPException, APIRouter, Query
+from fastapi import FastAPI, Form, Request, HTTPException, APIRouter, Query
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from backend.app.models.incidence import IncidenceOut
@@ -12,7 +12,7 @@ templates = Jinja2Templates(directory="src/backend/app/templates")  # Asegúrate
 
 @app.get('/consultar', response_class=HTMLResponse)
 def consultar(request: Request):
-    |||Renders the 'ConsultarIncidencia.html' template.|||
+    """Renders the 'ConsultarIncidencia.html' template."""
     return templates.TemplateResponse("ConsultarIncidencia.html", {"request": request})
 
 
@@ -23,7 +23,7 @@ async def get_all_incidences(
         skip: int = Query(0, ge=0),
         limit: int = Query(10, ge=1)
 ):
-    |||Retrieves all incidences from the database.|||
+    """Retrieves all incidences from the database."""
     filters = {"status": status} if status else {}
     incidences = controller.read_all(IncidenceOut, filters=filters, skip=skip, limit=limit)
     return incidences #changed to return incidences
@@ -32,7 +32,7 @@ async def get_all_incidences(
 
 @app.get("/incidencia/{IncidenciaID}", response_class=HTMLResponse)
 async def get_incidence_by_id(request: Request, IncidenciaID: int):
-    |||Retrieves an incidence by its ID and renders it using a template.|||
+    """Retrieves an incidence by its ID and renders it using a template."""
     incidence = controller.get_by_id(IncidenceOut, IncidenciaID)
     if not incidence:
         raise HTTPException(status_code=404, detail="Incidencia not found")  # Devuelve 404 si no se encuentra
@@ -43,4 +43,3 @@ async def get_incidence_by_id(request: Request, IncidenciaID: int):
         "Tipo": incidence.Tipo,
         "TicketID": incidence.TicketID
     })
-"""
