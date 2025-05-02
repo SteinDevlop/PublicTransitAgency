747f90e56cc735c49f3af42cc7c03bf3909f0915
Sinoombre <mjuliowilches@gmail.com>
2025-05-01 19:15:47 -0500

    query?

--- src/backend/app/test/api/test_maintainance_status_query_service.py
+++ src/backend/app/test/api/test_maintainance_status_query_service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from backend.app.api.routes.maintainance_status_query_service import app as maintainance_router
+from backend.app.api.routes.maintainance_status_CUD_service import app as maintainance_router
 from backend.app.logic.universal_controller_sql import UniversalController
 from backend.app.models.maintainance_status import MaintainanceStatusCreate, MaintainanceStatusOut, MaintainanceStatus
 from typing import List, Dict, Any
