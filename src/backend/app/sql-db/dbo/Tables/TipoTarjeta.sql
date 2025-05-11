723325dda3fc78f446e4f6802405f8e2832e3a88
Alejandro Pedro Steinman Cuesta <steinman920@gmail.com>
2025-05-10 19:54:30 -0500

    delete sql-db

--- src/backend/app/sql-db/dbo/Tables/TipoTarjeta.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[TipoTarjeta] (
-    [ID]   INT          NOT NULL,
-    [Tipo] VARCHAR (20) NOT NULL,
-    CONSTRAINT [PK_TipoTarjeta] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
-
-GO
-
--- src/backend/app/sql-db/dbo/Tables/TipoTransporte.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[TipoTransporte] (
-    [ID]             INT          NOT NULL,
-    [TipoTransporte] VARCHAR (20) NOT NULL,
-    CONSTRAINT [PK_TipoTransporte] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
-
-GO
-
