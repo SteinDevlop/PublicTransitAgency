95ed79bd3443cf05b08d5367bde0b0ba074b809d
DEV-ZERO-cloud <andresrubianomarrugo@gmail.com>
2025-05-11 17:19:58 -0500

    aa

--- src/backend/app/api/main.py
+++ src/backend/app/api/main.py
@@ -6,13 +6,7 @@ from backend.app.core.middlewares import add_middlewares
 from backend.app.api.routes import (
     incidence_cud_service,
     maintainance_status_query_service,
-    user_CUD_service,
-    type_movement_cud_service,
-    type_transport_cud_service,
-    rol_user_cud_service,
-    movement_cud_service,
     login_service,
-    price_cud_service,
     incidence_query_service,
     ticket_cud_service,
     ticket_query_service,
@@ -21,7 +15,6 @@ from backend.app.api.routes import (
     shifts_query_service,
     stops_cud_service,
     stops_query_service,
-    movement_query_service,
     payment_cud_service,
     payment_query_service,
     maintainance_status_cud_service,
@@ -30,11 +23,19 @@ from backend.app.api.routes import (
     schedule_cud_service,
     schedule_query_service,
     routes_query_service,
-    user_query_service
 )
 from backend.app.api.routes.card_service import (card_cud_service,card_query_service)
 from backend.app.api.routes.maintainance_service import (maintance_cud_service,maintance_query_service)
 from backend.app.api.routes.type_card_service import (type_card_cud_service,type_card_query_service)
+from backend.app.api.routes.user_service import (user_CUD_service,user_query_service)
+from backend.app.api.routes.movement_service import (movement_cud_service,movement_query_service)
+from backend.app.api.routes.price_service import (price_cud_service, price_query_service)
+from backend.app.api.routes.type_movement_service import (type_movement_cud_service, type_movement_query_service)
+from backend.app.api.routes.type_transport_service import (type_transport_cud_service,type_transport_query_service)
+from backend.app.api.routes.rol_user_service import (rol_user_cud_service,rol_user_query_service)
+from backend.app.api.routes.pqr_service import (pqr_cud_service,pqr_query_service)
+from backend.app.api.routes.asistance_service import (asistance_cud_service,asistance_query_service)
+from backend.app.api.routes.behavior_service import (behavior_cud_service,behavior_query_service)
 
 # Initialize the FastAPI app
 api_router = FastAPI(title=settings.PROJECT_NAME)
@@ -89,3 +90,14 @@ api_router.include_router(schedule_query_service.app)
 api_router.include_router(routes_query_service.app)
 api_router.include_router(user_query_service.app)
 api_router.include_router(routes_query_service.app)
+api_router.include_router(user_query_service.app)
+api_router.include_router(price_query_service.app)
+api_router.include_router(type_movement_query_service.app)
+api_router.include_router(type_transport_query_service.app)
+api_router.include_router(rol_user_query_service.app)
+api_router.include_router(pqr_query_service.app)
+api_router.include_router(pqr_cud_service.app)
+api_router.include_router(asistance_cud_service.app)
+api_router.include_router(asistance_query_service.app)
+api_router.include_router(behavior_cud_service.app)
+api_router.include_router(behavior_query_service.app)
