b5ad1b4b6e7edd1e334f4d26220155fe1b610fef
DEV-ZERO-cloud <andresrubianomarrugo@gmail.com>
2025-05-15 22:29:30 -0500

    servicerendimiento

--- src/backend/app/api/routes/behavior_service/behavior_cud_service.py
+++ src/backend/app/api/routes/behavior_service/behavior_cud_service.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 from backend.app.models.behavior import BehaviorCreate, BehaviorOut
-from backend.app.logic.universal_controller_sqlserver import UniversalController
+from backend.app.logic.universal_controller_instance import universal_controller as controller
 from backend.app.core.auth import get_current_user
 
 # Configuración de logging
@@ -14,71 +14,102 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 app = APIRouter(prefix="/behavior", tags=["behavior"])
-controller = UniversalController()
 templates = Jinja2Templates(directory="src/backend/app/templates")
 
 
-@app.get("/crear", response_class=HTMLResponse)
+@app.get("/supervisor/crear", response_class=HTMLResponse)
 def index_create(
     request: Request,
-    current_user: dict = Security(
-        get_current_user,
-        scopes=["system", "administrador", "supervisor"]
-    )
+    #current_user: dict = Security(
+        #get_current_user,
+        #scopes=["system", "administrador", "pasajero"])
 ):
-    logger.info(f"[GET /crear] Rendimiento: {current_user['user_id']} - Mostrando formulario de creación de rendimiento")
-    return templates.TemplateResponse("CrearRendimiento.html", {"request": request})
+    #logger.info(f"[GET /crear] Usuario: {current_user['user_id']} - Mostrando formulario de creación de PQR")
+    try:
+        behaviors = controller.read_all(BehaviorOut)
+        ultimo_id = max(p["ID"] for p in behaviors) if behaviors else 0
+        nuevo_id = ultimo_id + 1
+    except Exception as e:
+        logger.error(f"Error al obtener el último ID: {str(e)}")
+        nuevo_id = 1  # Por defecto
 
+    return templates.TemplateResponse("CrearSupervisorRendimiento.html", {
+        "request": request,
+        "nuevo_id": nuevo_id
+    })
 
-@app.get("/actualizar", response_class=HTMLResponse)
+@app.get("/administrador/crear", response_class=HTMLResponse)
+def index_create(
+    request: Request,
+    #current_user: dict = Security(
+        #get_current_user,
+        #scopes=["system", "administrador", "pasajero"])
+):
+    #logger.info(f"[GET /crear] Usuario: {current_user['user_id']} - Mostrando formulario de creación de PQR")
+    try:
+        behaviors = controller.read_all(BehaviorOut)
+        ultimo_id = max(p["ID"] for p in behaviors) if behaviors else 0
+        nuevo_id = ultimo_id + 1
+    except Exception as e:
+        logger.error(f"Error al obtener el último ID: {str(e)}")
+        nuevo_id = 1  # Por defecto
+
+    return templates.TemplateResponse("CrearAdministradorRendimiento.html", {
+        "request": request,
+        "nuevo_id": nuevo_id
+    })
+
+@app.get("/administrador/actualizar", response_class=HTMLResponse)
 def index_update(
     request: Request,
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador","supervisor"])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador","supervisor"])
 ):
-    logger.info(f"[GET /actualizar] Rendimiento: {current_user['user_id']} - Mostrando formulario de actualización de rendimiento")
+    #logger.info(f"[GET /actualizar] Rendimiento: {current_user['user_id']} - Mostrando formulario de actualización de rendimiento")
     return templates.TemplateResponse("ActualizarRendimiento.html", {"request": request})
 
 
-@app.get("/eliminar", response_class=HTMLResponse)
+@app.get("/administrador/eliminar", response_class=HTMLResponse)
 def index_delete(
     request: Request,
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
-    logger.info(f"[GET /eliminar] Rendimiento: {current_user['user_id']} - Mostrando formulario de eliminación de rendimiento")
+    #logger.info(f"[GET /eliminar] Rendimiento: {current_user['user_id']} - Mostrando formulario de eliminación de rendimiento")
     return templates.TemplateResponse("EliminarRendimiento.html", {"request": request})
 
 
 @app.post("/create")
 async def create_behavior(
-    id: int = Form(...),
+    request: Request,
+    ID: int = Form(...),
     iduser:int= Form(...),
     cantidadrutas: int=Form(...),
     horastrabajadas: int=Form(...),
     observaciones:str=Form(...),
     fecha: str = Form(...),
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
-    logger.info(f"[POST /create] Behavior: {current_user['user_id']} - Intentando crear rendimiento con id: {id}")
+    #logger.info(f"[POST /create] Behavior: {current_user['user_id']} - Intentando crear rendimiento con ID: {ID}")
 
     try:
         # Verificar si el rendimiento ya existe
-        existing_behavior = controller.get_by_column(BehaviorOut, "id", id)  
+        existing_behavior = controller.get_by_id(BehaviorOut, ID)  
         if existing_behavior:
-            logger.warning(f"[POST /create] Error de validación: El rendimiento ya existe con identificación {id}")
+            logger.warning(f"[POST /create] Error de validación: El rendimiento ya existe con identificación {ID}")
             raise HTTPException(400, detail="El rendimiento ya existe con la misma identificación.")
 
         # Crear rendimiento
-        new_behavior = BehaviorCreate(id=id, iduser=iduser,cantidadrutas=cantidadrutas, horastrabajadas=horastrabajadas,observaciones=observaciones,fecha=fecha)
-        logger.info(f"Intentando insertar rendimiento con datos: {new_behavior.model_dump()}")
+        new_behavior = BehaviorCreate(ID=ID, iduser=iduser,cantidadrutas=cantidadrutas, horastrabajadas=horastrabajadas,observaciones=observaciones,fecha=fecha)
         controller.add(new_behavior)
-        logger.info(f"Rendimiento insertado con ID: {new_behavior.id}")  # Verifica si el ID se asigna
-        logger.info(f"[POST /create] Rendimiento creado exitosamente con identificación {id}")
-        return {
+        logger.info(f"Rendimiento insertado con ID: {new_behavior.ID}")  # Verifica si el ID se asigna
+        logger.info(f"[POST /create] Rendimiento creado exitosamente con identificación {ID}")
+        context =  {
+            "request":request,
             "operation": "create",
             "success": True,
-            "data": BehaviorOut(id=new_behavior.id,iduser=new_behavior.iduser,cantidadrutas=new_behavior.cantidadrutas,horastrabajadas=new_behavior.horastrabajadas,observaciones=new_behavior.observaciones,fecha=new_behavior.fecha).model_dump(),
+            "data": BehaviorOut(ID=new_behavior.ID,iduser=new_behavior.iduser,cantidadrutas=new_behavior.cantidadrutas,horastrabajadas=new_behavior.horastrabajadas,observaciones=new_behavior.observaciones,fecha=new_behavior.fecha).model_dump(),
             "message": "Behavior created successfully."
         }
+        return templates.TemplateResponse("Confirmacion.html", context)
         
     except ValueError as e:
         logger.warning(f"[POST /create] Error de validación: {str(e)}")
@@ -90,35 +121,38 @@ async def create_behavior(
 
 @app.post("/update")
 async def update_behavior(
-    id: int = Form(...),
+    request: Request,
+    ID: int = Form(...),
     iduser:int= Form(...),
     cantidadrutas: int=Form(...),
     horastrabajadas: int=Form(...),
     observaciones:str=Form(...),
     fecha: str = Form(...),
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
-    logger.info(f"[POST /update] Rendimiento: {current_user['user_id']} - Actualizando rendimiento id={id}")
+    #logger.info(f"[POST /update] Rendimiento: {current_user['user_id']} - Actualizando rendimiento ID={ID}")
     try:
-        existing = controller.get_by_column(BehaviorOut,"id" ,id)
+        existing = controller.get_by_column(BehaviorOut,"ID" ,ID)
         if existing is None:
-            logger.warning(f"[POST /update] Rendimiento no encontrada: id={id}")
+            logger.warning(f"[POST /update] Rendimiento no encontrada: ID={ID}")
             raise HTTPException(404, detail="Behavior not found")
 
-        updated_behavior = BehaviorOut(id=id, iduser=iduser,cantidadrutas=cantidadrutas,
+        updated_behavior = BehaviorOut(ID=ID, iduser=iduser,cantidadrutas=cantidadrutas,
                                        horastrabajadas=horastrabajadas,
                                        observaciones=observaciones,fecha=fecha)
         controller.update(updated_behavior)
         logger.info(f"[POST /update] Rendimiento actualizada exitosamente: {updated_behavior}")
-        return {
+        context = {
+            "request":request,
             "operation": "update",
             "success": True,
-            "data": BehaviorOut(id=id, iduser=updated_behavior.iduser,horastrabajadas=updated_behavior.horastrabajadas, 
+            "data": BehaviorOut(ID=ID, iduser=updated_behavior.iduser,horastrabajadas=updated_behavior.horastrabajadas, 
                                 cantidadrutas=updated_behavior.cantidadrutas,
                                 observaciones=updated_behavior.observaciones,
                                 fecha=updated_behavior.fecha).model_dump(),
-            "message": f"Behavior {id} updated successfully."
+            "message": f"Behavior {ID} updated successfully."
         }
+        return templates.TemplateResponse("Confirmacion.html", context)
     except ValueError as e:
         logger.warning(f"[POST /update] Error de validación: {str(e)}")
         raise HTTPException(400, detail=str(e))
@@ -127,24 +161,28 @@ async def update_behavior(
 
 @app.post("/delete")
 async def delete_behavior(
-    id: int = Form(...),
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    request:Request,
+    ID: int = Form(...),
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
-    logger.info(f"[POST /delete] Rendimiento: {current_user['user_id']} - Eliminando rendimiento id={id}")
+    #logger.info(f"[POST /delete] Rendimiento: {current_user['user_id']} - Eliminando rendimiento ID={ID}")
     try:
-        existing = controller.get_by_column(BehaviorOut,"id",id)
+        existing = controller.get_by_column(BehaviorOut,"ID",ID)
         if not existing:
-            logger.warning(f"[POST /delete] Rendimiento no encontrado en la base de datos: id={id}")
+            logger.warning(f"[POST /delete] Rendimiento no encontrado en la base de datos: ID={ID}")
             raise HTTPException(404, detail="Behavior not found")
 
-        logger.info(f"[POST /delete] Eliminando rendimiento con id={id}")
+        logger.info(f"[POST /delete] Eliminando rendimiento con ID={ID}")
         controller.delete(existing) 
-        logger.info(f"[POST /delete] Rendimiento eliminada exitosamente: id={id}")
-        return {
+        logger.info(f"[POST /delete] Rendimiento eliminada exitosamente: ID={ID}")
+        context= {
+            "request":request,
             "operation": "delete",
             "success": True,
-            "message": f"Behavior {id} deleted successfully."
+            "message": f"Behavior {ID} deleted successfully."
         }
+        return templates.TemplateResponse("Confirmacion.html", context)
+    
     except HTTPException as e:
         raise e
     except Exception as e:
--- src/backend/app/api/routes/behavior_service/behavior_query_service.py
+++ src/backend/app/api/routes/behavior_service/behavior_query_service.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 from backend.app.core.auth import get_current_user
 from backend.app.models.behavior import BehaviorOut
-from backend.app.logic.universal_controller_sqlserver import UniversalController
+from backend.app.logic.universal_controller_instance import universal_controller as controller
 
 # Configuración del logger
 logger = logging.getLogger(__name__)
@@ -15,74 +15,130 @@ logging.basicConfig(level=logging.INFO)
 app = APIRouter(prefix="/behavior", tags=["behavior"])
 
 # Initialize universal controller instance
-controller = UniversalController()
 
 # Setup Jinja2 template engine
 templates = Jinja2Templates(directory="src/backend/app/templates")
 
 
-@app.get("/consultar", response_class=HTMLResponse)
+@app.get("/consultar/administrador", response_class=HTMLResponse)
 def consultar(
     request: Request,
-    current_user: dict = Security(get_current_user, scopes=[
-        "system", "administrador"
-    ])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"
+    #])
 ):
     """
     Render the 'ConsultarRendimiento.html' template for the behavior consultation page.
     """
-    logger.info(f"[GET /consultar] Usuario: {current_user['user_id']} - Mostrando página de consulta de rendimiento")
-    return templates.TemplateResponse(request,"ConsultarRendimiento.html", {"request": request})
+    #logger.info(f"[GET /consultar] Usuario: {current_user['user_id']} - Mostrando página de consulta de rendimiento")
+    return templates.TemplateResponse(request,"ConsultarRendimientoViaAdministrador.html", {"request": request})
 
-@app.get("/consultar/user", response_class=HTMLResponse)
+@app.get("/consultar/supervisor", response_class=HTMLResponse)
 def consultar(
     request: Request,
-    current_user: dict = Security(get_current_user, scopes=[
-        "system", "administrador"
-    ])
+    #current_user: dict = Security(get_current_user, scopes=[
+        #"system", "administrador"])
 ):
     """
     Render the 'ConsultarPQR.html' template for the pqr consultation page.
     """
-    logger.info(f"[GET /consultar] Usuario: {current_user['user_id']} - Mostrando página de consulta de pqr")
-    return templates.TemplateResponse(request,"ConsultarRendimientoUsuario.html", {"request": request})
+    #logger.info(f"[GET /consultar] Usuario: {current_user['user_id']} - Mostrando página de consulta de pqr")
+    return templates.TemplateResponse(request,"ConsultarRendimientoViaSupervisor.html", {"request": request})
 
+@app.get("/supervisor/behaviors")
+async def get_behaviors(
+    request:Request
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+):
+    """
+    Retrieve and return all behaviors records from the database.
+    """
+    #logger.info(f"[GET /pqrs] Usuario: {current_user['user_id']} - Consultando todas las behaviors.")
+    behaviors = controller.read_all(BehaviorOut)
+    if behaviors:
+        # Si hay varias asistencias, iterar sobre ellas
+        context = {
+            "request": request,
+            "behaviors": behaviors,  # Lista de behaviors
+        }
+    else:
+        logger.warning(f"[GET /pqrs] No se encontraron usuarios registrados")
+        context = {
+            "request": request,
+            "behaviors": behaviors  # Si no se encontraron usuarios
+        }
+    return templates.TemplateResponse("Supervisorbehaviors.html", context)
 
+@app.get("/administrador/behaviors")
+async def get_behaviors(
+    request:Request
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+):
+    """
+    Retrieve and return all behaviors records from the database.
+    """
+    #logger.info(f"[GET /pqrs] Usuario: {current_user['user_id']} - Consultando todas las behaviors.")
+    behaviors = controller.read_all(BehaviorOut)
+    if behaviors:
+        # Si hay varias asistencias, iterar sobre ellas
+        context = {
+            "request": request,
+            "behaviors": behaviors,  # Lista de behaviors
+        }
+    else:
+        logger.warning(f"[GET /pqrs] No se encontraron usuarios registrados")
+        context = {
+            "request": request,
+            "behaviors": behaviors  # Si no se encontraron usuarios
+        }
+    return templates.TemplateResponse("Administradorbehaviors.html", context)
 
 @app.get("/rendimientos")
 async def get_rendimientos(
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    request:Request,
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
     """
     Retrieve and return all behavior records from the database.
     """
-    logger.info(f"[GET /rendimientos] Usuario: {current_user['user_id']} - Consultando todas las rendimientos.")
+    #logger.info(f"[GET /rendimientos] Usuario: {current_user['user_id']} - Consultando todas las rendimientos.")
     rendimientos = controller.read_all(BehaviorOut)
     logger.info(f"[GET /rendimientos] Número de rendimientos encontradas: {len(rendimientos)}")
-    return rendimientos
+    if rendimientos:
+        # Si hay varias asistencias, iterar sobre ellas
+        context = {
+            "request": request,
+            "rendimientos": rendimientos,  # Lista de asistencias
+        }
+    else:
+        context = {
+            "request": request,
+            "rendimientos": rendimientos  # Si no se encontraron asistencias, pasar una lista vacía
+        }
 
-#behavior by id behavior
-@app.get("/rendimiento", response_class=HTMLResponse)
+    return templates.TemplateResponse("rendimientos.html", context)
+
+#behavior by ID behavior
+@app.get("/byid", response_class=HTMLResponse)
 def rendimiento_by_id(
     request: Request,
-    id: int =Query(...),
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
+    ID: int =Query(...),
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador"])
 ):
     """
     Retrieve a behavior by its ID and render the 'rendimiento.html' template with its details.
     If the behavior is not found, display 'None' for all fields.
     """
-    logger.info(f"[GET /rendimiento] Usuario: {current_user['user_id']} - Consultando rendimiento con id={id}")
-    unit_rendimiento = controller.get_by_id(BehaviorOut, id)
+    #logger.info(f"[GET /rendimiento] Usuario: {current_user['user_id']} - Consultando rendimiento con ID={ID}")
+    unit_rendimiento = controller.get_by_id(BehaviorOut, ID)
 
     if unit_rendimiento:
-        logger.info(f"[GET /rendimiento] Rendimiento encontrada: {unit_rendimiento.id}, iduser: {unit_rendimiento.iduser}")
+        logger.info(f"[GET /rendimiento] Rendimiento encontrada: {unit_rendimiento.ID}, iduser: {unit_rendimiento.iduser}")
     else:
-        logger.warning(f"[GET /rendimiento] No se encontró rendimiento con id={id}")
+        logger.warning(f"[GET /rendimiento] No se encontró rendimiento con ID={ID}")
 
     context = {
         "request": request,
-        "id": unit_rendimiento.id if unit_rendimiento else "None",
+        "ID": unit_rendimiento.ID if unit_rendimiento else "None",
         "iduser": unit_rendimiento.iduser if unit_rendimiento else "None",
         "cantidadrutas": unit_rendimiento.cantidadrutas if unit_rendimiento else "None",
         "horastrabajadas": unit_rendimiento.horastrabajadas if unit_rendimiento else "None",
@@ -92,30 +148,30 @@ def rendimiento_by_id(
 
     return templates.TemplateResponse(request,"rendimiento.html", context)
 
-#behavior by user id
-@app.get("/user", response_class=HTMLResponse)
+#behavior by user ID
+@app.get("/byuser", response_class=HTMLResponse)
 def rendimiento_by_user(
     request: Request,
     iduser: int = Query(...),
-    current_user: dict = Security(get_current_user, scopes=["system", "administrador","supervisor"])
+    #current_user: dict = Security(get_current_user, scopes=["system", "administrador","supervisor"])
 ):
     """
     Retrieve a behavior by its ID and render the 'rendimiento.html' template with its details.
     If the behavior is not found, display 'None' for all fields.
     """
-    logger.info(f"[GET /rendimiento] Usuario: {current_user['user_id']} - Consultando rendimiento con id={iduser}")
+    #logger.info(f"[GET /rendimiento] Usuario: {current_user['user_id']} - Consultando rendimiento con ID={iduser}")
     unit_rendimiento = controller.get_by_column(BehaviorOut, column_name="iduser", value = iduser)
 
     if unit_rendimiento:
         # Si hay varias asistencias, iterar sobre ellas
         context = {
             "request": request,
-            "asistencias": unit_rendimiento,  # Lista de asistencias
+            "rendimientos": unit_rendimiento,  # Lista de asistencias
         }
     else:
         logger.warning(f"[GET /asistencia] No se encontraron asistencias con iduser={iduser}")
         context = {
             "request": request,
-            "rendimientos_list": []  # Si no se encontraron asistencias, pasar una lista vacía
+            "rendimientos": unit_rendimiento # Si no se encontraron asistencias, pasar una lista vacía
         }
     return templates.TemplateResponse(request,"rendimientos.html", context)
